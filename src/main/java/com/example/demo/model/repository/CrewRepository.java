91952a82fa223cc4cc191f84462fc4a2101138d3
Andrew <rmguy24@gmail.com>
2023-02-21 20:52:30 +0300

    Flower shop pet project code

--- src/main/java/com/example/demo/model/repository/CrewRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.model.repository;
-
-import com.example.demo.model.entity.Crew;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CrewRepository extends JpaRepository<Crew,Long> {
-}
--- src/main/java/com/example/demo/model/repository/ShipRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.model.repository;
-
-import com.example.demo.model.entity.Ship;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ShipRepository extends JpaRepository<Ship,Long> {
-}
